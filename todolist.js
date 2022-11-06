e2f145288e63149cce47baa5c3ee731e0a55d83b
Deepak Thakur <deepthakur886210@gmail.com>
2022-11-06 12:13:45 +0530

    update

--- todolist.js
+++ todolist.js
@@ -1,28 +1,3 @@
-// const createTask = document.getElementsByClassName("btn1");
-// const serachbar = document.getElementsByClassName('search-wrapper');
-// const drop = document.getElementsByClassName("dropdown");
-// const calender = document.getElementsByClassName("date");
-// const myList = document.getElementsByClassName("list");
-
-// function tableCreate(table_id){
-//     let table_body = document.getElementById("table_id"),
-//         first_tr = table_body.firstElementChild
-//         tr_clone = first_tr.cloneNode(true);
-//         table_body.append(tr_clone);
-
-//         // clean_first_tr(table_body.firstElementChild);
-// }
-
-// function remove_tr(This) {
-//     if(This.closest('tbody').childElementCount == 1)
-//     {
-//         alert("You Don't have Permission to Delete This");
-//     }else{
-//         This.closest('tr').remove();
-//     }
-
-// }
-
 let nameinput = document.querySelector(".name_input");
 let dateinput = document.querySelector(".date_input");
 let taskbutton = document.querySelector(".taskbutton");
@@ -33,15 +8,62 @@ taskbutton.addEventListener("click", ()=> {
  console.log(taskArray);
   adtodo();
 });
-// dateinput.addEventListener("click", ()=> {
-//     console.log(dateinput.value);
-//   taskArray.push(dateinput.value);
-//   adtodo();
-// });
 function adtodo() {
     mylist.innerHTML = "" ; 
     taskArray.map((item) => {
         // console.log(item)
-        mylist.innerHTML += ``;  
+        mylist.innerHTML += ` <div class="d-flex justify-content-around mt-4 ">
+        <label>${item.name}</label>
+        <label>${item.date}</label>
+        <div>Remaining</div>
+        <button class= "details_class" type="button" onclick=\'${detailfun(item)}\' class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal">
+ Detail
+</button>
+        <button class=" delete_btn"><i class="fa-solid fa-xmark"></i></button>
+    </div>`;  
     })
+    let remove = document.querySelectorAll(".delete_btn");
+    let removarr = Array.from(remove);  
+
+    removarr.map((button, index) =>{   
+        button.addEventListener("click", function(){
+            taskArray.splice(index, 1); 
+            adtodo() 
+        });
+    });
+
+function detailfun(item) {
+  console.log("item name is " + item.name )
+  document.getElementsByClassName("details_class").innerHTML =  `<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="container">
+      <div class="flex">
+        <h1>Name : </h1>
+        <h2>${item.name}</h2>
+      </div>
+      <div class="flex">
+        <h1>Date : </h1>
+        <h2>${item.date}</h2>
+      </div>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>`;;
+
+
+
+}  
 }
